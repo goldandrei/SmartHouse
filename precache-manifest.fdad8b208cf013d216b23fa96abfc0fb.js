bc29a76dd1fc99c88f7f4e25f670e9754a0f8451
goldandrei <goldenbergandrei@gmail.com>
2020-05-10 19:56:16 +0300

    Updates

---
+++ precache-manifest.fdad8b208cf013d216b23fa96abfc0fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d84cfec7d968368b41f148190c6235b",
+    "url": "/SmartHouse/index.html"
+  },
+  {
+    "revision": "0abb6eb3fca7ddd50932",
+    "url": "/SmartHouse/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "c0d0a1298acaef3044b5",
+    "url": "/SmartHouse/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0abb6eb3fca7ddd50932",
+    "url": "/SmartHouse/static/js/2.ac58174f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/SmartHouse/static/js/2.ac58174f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0d0a1298acaef3044b5",
+    "url": "/SmartHouse/static/js/main.2734620c.chunk.js"
+  },
+  {
+    "revision": "f4bf78864d6e25e34983",
+    "url": "/SmartHouse/static/js/runtime-main.231ba8c8.js"
+  }
+]);
